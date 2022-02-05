f71f12f6f12aff0cff6bf9f8906874bdc18921a4
Максим Крупин <buttercuper@yandex.ru>
2022-02-05 15:23:34 +0300

    Translate main page(3)

--- components/GalleryText.js
+++ components/GalleryText.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { useState , useEffect } from "react";
 import styles from "../styles/Gallary.module.css"
 import GallaryPoint from "./GallaryPoint"
 
