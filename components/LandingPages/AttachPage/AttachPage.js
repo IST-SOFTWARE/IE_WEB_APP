eff12c525b73ef0c031c68adbac11f5df519a5b3
Максим Крупин <buttercuper@yandex.ru>
2022-03-03 23:43:51 +0300

    Input form done

--- components/LandingPages/AttachPage/AttachPage.js
+++ components/LandingPages/AttachPage/AttachPage.js
@@ -3,6 +3,7 @@ import Image from "next/image"
 import MainLabel from "../../MainLabel"
 import ATContentBlock from "./APContentBlock"
 import APAttachBlock from "./APAttachBlock"
+import InputItem from "./InputItem"
 
 export default function AttachPage(){
     return(
@@ -16,12 +17,37 @@ export default function AttachPage(){
                         ЭСКИЗАМ 
                         </MainLabel>
                         
-                        <ATContentBlock StepNum={1}>
-                            <APAttachBlock/>
-                        </ATContentBlock>
+                        <div className={styles.AttachBlockParent}>
+                            <ATContentBlock StepNum={1}>
+                                <APAttachBlock/>
+                            </ATContentBlock>
+                        </div>
 
                     </div>
+                    
                     <div className={styles.RightContent}>
+
+                    <div className={styles.UserInputBlockParent}>
+                        <ATContentBlock StepNum={2}>
+
+                                <div className={styles.ABContainerInput}>   
+
+                                    <div className={styles.ABHeaderInput}>
+                                        <p>Регистрация:</p>
+                                        <a>Уже зарегистрирован...</a>
+                                    </div>
+
+                                        <InputItem Title={"ИНН Организации"} Placeholder={"1111111111"} id="1"/>
+                                        <InputItem Title={"Название компании"} Placeholder={"ОТИС Лифт"} id="2"/>
+                                        <InputItem Title={"ФИО представителя"} Placeholder={"Андреев Андрей Андреевич"} id="3"/>
+                                        <InputItem Title={"Должность представителя"} Placeholder={"Менеджер по закупкам"} id="4"/>
+                                        <InputItem Title={"Контактный телефон"} Placeholder={"+7(000)000 00 00"} id="5"/>
+                                        <InputItem Title={"Email"} Placeholder={"info@istlift.ru"} id="6"/>
+
+                                </div>
+                            </ATContentBlock>
+                        </div>
+
                     </div>     
 
                 <div className={styles.AttachContentBg}>
---
+++ components/LandingPages/AttachPage/InputItem.js
@@ -0,0 +1,156 @@
+import styles from "../../../styles/AttachPage.module.css"
+import { useState, useEffect, useReducer, useMemo, useCallback} from "react"
+
+
+export default function InputItem({Title, Placeholder}){
+
+    //BOILER PLATE`S
+    const INPUT_BP = "Input";
+    const FOCUS_BP = "focus";
+    const CONFIRMED_BP = "Confirmed";
+    
+    //SHOW / HIDE INPUT
+    const[showInput, setShowInput] = useState(true);
+    
+    //EDIT-FOCUSER
+    const[focuser, setFocus] = useState(false);
+
+    //INPUT BASE ELEMENT 
+    const[state, dispatch] = useReducer(reducer,{
+        Value: "",
+        Focus: false,
+        Form: {},
+        Confirmed: true
+    });
+
+    //INPUT REDUSER
+    function reducer(state, action){
+        switch(action.type){
+            case INPUT_BP:
+                return{
+                        Value: action.payload,
+                        Focus: true,
+                        Form: action.obj,
+                        Confirmed: state.Confirmed
+                }
+            case FOCUS_BP:
+                // console.log("focus");
+                return{
+                    Value: state.Value,
+                    Focus: action.payload,
+                    Form: action.obj,
+                    Confirmed: state.Confirmed
+                }
+            case CONFIRMED_BP:
+                return{
+                    Value: state.Value,
+                    Focus: state.Focus,
+                    Form: state.Form,
+                    Confirmed: action.payload
+                }
+        }
+    }
+
+
+    // -- [ACTION-GENERATOR`S] --
+    const OnChangerGenerator = (event) => {
+        return{
+            type: INPUT_BP,
+            payload: event.target.value,
+            obj: event
+        }
+    };
+
+    const OnFocusGenerator = (focus, obj) => {
+        // console.log(obj.target);
+        return{
+            type: FOCUS_BP,
+            payload: focus,
+            obj: obj
+        }
+    }
+
+    const setConfirmedGenerator = (conf) =>{
+        return{
+            type: CONFIRMED_BP,
+            payload: conf
+        }
+    }
+    // --[]--
+
+
+    // BASE EVENTS LISTENER [FOCUSED, CONFIRMED]
+    useEffect(()=>{
+        if(state.Focus){
+            state.Form.target.className = `${styles.ActiveInputForm}`;
+            dispatch(setConfirmedGenerator(true));
+        }
+        else{
+            if(state.Form.target !== undefined){
+                if(state.Value.length > 0){
+                    state.Form.target.classList.remove(`${styles.ActiveInputForm}`);
+                    dispatch(setConfirmedGenerator(true));
+                    setShowInput(false);
+                    setFocus(false);
+               } 
+                if(state.Value.length === 0){
+                    state.Form.target.classList.remove(`${styles.ActiveInputForm}`);
+                    state.Form.target.className = `${styles.shouldBeFilled}`;
+                    dispatch(setConfirmedGenerator(false));
+                    setShowInput(true);
+                } 
+            }
+        }
+    }, [state.Value, state.Focus, state.Form])
+
+
+    // HANDLER FOR EDIT
+    function EditField(){
+        setShowInput(true);
+        setFocus(true)
+    }
+
+    // EDIT ? SET FOCUS
+    useEffect(() => {
+        if(state.Form.target !== undefined && state.Value.length > 0 && focuser){
+            const EditElem = document.getElementById(`input_value_${state.Value}`)
+            EditElem.focus();
+        }
+    },[focuser])
+
+
+    return(
+        <>  
+                <div className={styles.InputBlock}>
+                    <label className =
+                    {state.Confirmed ? null
+                    : `${styles.isNotConfirmed}`}>
+
+                        {Title + ":"}
+
+                    </label>
+                    
+                    {showInput ?  
+                    <input
+                        id={`input_value_${state.Value}`}
+                        value={state.Value}
+                        onChange={(e) => dispatch(OnChangerGenerator(e))}
+                        onFocus={(e) => dispatch(OnFocusGenerator(true, e))}
+                        onBlur={(e) => dispatch(OnFocusGenerator(false, e))}
+                        type="text"
+                        placeholder={Placeholder}
+                    >
+                    </input>
+                    :
+    
+                    <p className={styles.ConfirmedInputData}
+                    onClick={() => EditField()}>
+                        {state.Value}
+                    </p>
+    
+                    }
+                </div>
+
+        </>
+    )
+}
