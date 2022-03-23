0a491dcd43bae1e52b58186f72839d2fcf35f83e
Максим Крупин <buttercuper@yandex.ru>
2022-03-23 17:42:31 +0300

    add bootstrap-grid local

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,6 @@ export default function MyApp({Component, PageProps}){
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
-            <link rel="stylesheet" href="https://res.cloudinary.com/dv9xitsjg/raw/upload/v1648036359/Bootstrap/bootstrap-grid.min_qafb3t.css"/>
         </Head>
         <NextNProgress
         color="#29D"
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+      <link rel="stylesheet" href="/Bootstrap/bootstrap-grid.min.css"/>
+      </Head>
       <body>
         <div id="PopUpBase"/>
         <div id="CatalogSpace"/>
