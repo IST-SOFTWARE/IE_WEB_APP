99dfaab16a93ae27d0b7d01f3d2bb5424e454dea
Максим Крупин <buttercuper@yandex.ru>
2022-03-24 17:42:25 +0300

    remade save ratio component for next-image

--- components/NextImageRatioSaver.js
+++ components/NextImageRatioSaver.js
@@ -4,68 +4,83 @@ import { useState, useEffect, useCallback} from "react";
 export default function NextImageRatioSaver({Img, id, wPrime, hPrime}){
     const[lilImgLoad, setLilLoad] = useState(false);
     const[bigImgContLoad, setBigContLoad] = useState(false);
+    const[CalibrationBlockEx, setEx] = useState(false);
 
     const[imageSize, setSize] = useState({
         width: 0,
         height: 0
     })
 
-    const updateSize = useCallback(
-        () => {
-            const BigImgCont = document.querySelector(`.${"NextImageRatioSaver_" + id}`);
+    const[calibratedSize, setCalibrated] = useState({
+        width: 0,
+        height: 0
+    })
 
- 
-            const BI_Height = BigImgCont.offsetHeight;
-            const BI_Width = BigImgCont.offsetWidth;
-
-                if((wPrime !== 0 || wPrime !== false) && (hPrime === undefined || hPrime === null)){
-                    BigImgCont.style.height = ((imageSize.width/imageSize.height) * BI_Width).toString() + "px"
-                    console.log(BI_Height, BI_Width);
-                }
-                else if((hPrime !== 0 || hPrime !== false) && (wPrime === undefined || wPrime === null)){
-                    BigImgCont.style.width = ((imageSize.width/imageSize.height) * BI_Height).toString() + "px"
-                    console.log(BI_Height, BI_Width);
-                }
+    useEffect(()=>{
+        const CalibrationItem = document.querySelector(".CalibrationBlock");
+        // console.log(CalibrationItemEx);
         
-        },
-        [bigImgContLoad, lilImgLoad],
-      );
+        if(!CalibrationItem.classList.contains(".Ex")){
+        CalibrationItem.classList.add("Ex");
+
+        if((wPrime !== 0 || wPrime !== false) && (hPrime === undefined || hPrime === null)){
+           
+            setCalibrated({
+                width: CalibrationItem.offsetWidth,
+                height: (imageSize.width/imageSize.height) * CalibrationItem.offsetWidth
+            })
+        }
+        else if((hPrime !== 0 || hPrime !== false) && (wPrime === undefined || wPrime === null)){
+        
+            setCalibrated({
+                width: (imageSize.width/imageSize.height) * CalibrationItem.offsetHeight,
+                height: CalibrationItem.offsetHeight
+            })
+        }
+
+            console.log("SIZES: ", CalibrationItem.offsetWidth, CalibrationItem.offsetHeight)
+        }
+
+    },[imageSize])
+
 
     useEffect(()=>{
     if(lilImgLoad){
-       const lImg = document.querySelector("#lil_img_" + id);
+       const lImg = document.querySelector("#lil_img_");
        setSize(
         {
             width: lImg.naturalWidth,
             height: lImg.naturalHeight
         })
-
-       
-        // console.log("IMG_SIZE: ", lImg);
     }
     },[lilImgLoad]);
 
-    useEffect(()=>{
-        updateSize();
-    },[bigImgContLoad])
 
     return(
         <>
+            <div style={{position: `relative`, width:`100%`, height:`100%`}}>
                 <img src={Img} width="0px" style={{position: `absolute`}}
-                id={"lil_img_" + id}
+                id={"lil_img_"}
                 onLoad={()=>setLilLoad(true)}/>
-
-                <div className={"NextImageRatioSaver_" + id}
+                    
+                <div className="CalibrationBlock"
                     style={{width: `100%`,
                     height: `100%`,
-                    position: `relative`}}
-                    onLoad={()=>setBigContLoad(true)}>
+                    position: `absolute`}}>
+                </div>
+
+                <div className={"NextImageRatioSaver"}
+                    style={{width: calibratedSize.width + `px`,
+                    height: calibratedSize.height + `px`,
+                    position: `relative`}}>
 
                     <Image
                     src={Img}
                     layout="fill"
-                    />
+                /> 
                 </div>
+               
+            </div>
         </>
     )
 }
