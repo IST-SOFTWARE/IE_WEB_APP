fc87f2da6248d0ddf2472d58e63dbb781435b73d
Eugene Domanovich <dompro1994.ed@gmail.com>
2023-03-03 18:40:38 +0300

    task: filters. not complet "dot"

---
+++ components/UI/ICheckBoxList/Filter.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import styles from "./checkBoxList.module.scss";
+import { maxLengthText } from "../common";
+
+const Filter = ({ field }) => {
+  const [hasCheked, setHasCheked] = useState(field.isActive);
+  const [openDiscrabing, setOpenDiscrabing] = useState(false);
+
+  const activeFilterCheck = () => {
+    hasCheked ? setHasCheked(false) : setHasCheked(true);
+  };
+
+  const activeDiscrabing = (e) => {
+    if (field.fieldName?.length >= 23) {
+      setTimeout(() => {
+        openDiscrabing ? setOpenDiscrabing(false) : setOpenDiscrabing(true);
+      }, 500);
+    }
+  };
+
+  return (
+    <div
+      className={`${styles.filter}`}
+      onClick={(e) => {
+        activeFilterCheck;
+      }}
+      onMouseEnter={activeDiscrabing}
+      onMouseLeave={(e) => {
+        activeDiscrabing;
+      }}
+    >
+      <div className={styles.columnField}>
+        <div
+          className={`${styles.fieldName} ${
+            hasCheked ? styles.activeText : ""
+          }`}
+        >
+          {maxLengthText(field.fieldName, 23)}
+        </div>
+        {openDiscrabing ? (
+          <span className={styles.discrabingFilter}>{field.fieldName}</span>
+        ) : null}
+      </div>
+
+      {field.checkBox ? (
+        <div
+          onClick={activeFilterCheck}
+          className={`${styles.checkPoint} ${hasCheked ? "" : styles.active}`}
+        ></div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Filter;
