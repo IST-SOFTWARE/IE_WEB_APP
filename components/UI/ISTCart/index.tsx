7ba3d7da073e5617a7089bcdfcfef86fc4e20a7b
Eugene Domanovich <dompro1994.ed@gmail.com>
2023-05-20 12:38:11 +0300

    create ISTCart component

---
+++ components/UI/ISTCart/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./index.module.scss";
+import IstButtonN from "../ISTButton/ISTButtonN";
+
+const ISTCart = () => {
+  return (
+    <div className={styles.Cart}>
+      <div className={styles.cartLabel}>Ваша Корзина 4</div>
+      <div className={styles.box}>
+        <div className={styles.prod}>
+          <div className={styles.title}>Товары (4)</div>
+          <div className={styles.total}>1 431 000₽</div>
+        </div>
+        <div className={styles.sale}>
+          <div className={styles.title}>Скидка</div>
+          <div className={styles.total}>10 845₽</div>
+        </div>
+      </div>
+      <div className={styles.totalSum}>Сумма: 1 420 155₽</div>
+      <IstButtonN
+        light={{
+          fill: true,
+          style: {
+            borderRadius: "15px",
+            fillContainer: true,
+          },
+        }}
+        title={{
+          caption: "Оформить заказ",
+        }}
+      />
+    </div>
+  );
+};
+
+export default ISTCart;
